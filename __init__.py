588e00f07a5ef6bd3bbe54a4f9a30d53c2222596
liuyq <lynuts@qq.com>
2026-03-19 18:09:29 +0800

    Refactor the interface

--- __init__.py
+++ __init__.py
@@ -1,18 +1,10 @@
 from .core.base import PMT_Fitter
 from .core.combined import CombinedFitter
 
-from .models.GaussFamily import *
-from .models.PolyaFamily import *
+from .models import Tweedie_Fitter, Gen_Tweedie_Fitter
 
 __all__ = [
-    "PMT_Fitter",
-    "BiGauss_Fitter",
-    "Linear_Gauss_Fitter",
-    "TriGauss_Fitter",
-    "Gauss_Compound_Fitter",
-    "BiPolya_Fitter",
-    "Polya_Exp_Fitter",
-    "Gamma_Tweedie_Fitter",
-    "Recursive_Polya_Fitter",
+    "Tweedie_Fitter",
+    "Gen_Tweedie_Fitter",
     "CombinedFitter",
 ]
